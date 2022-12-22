0916654f67377f6e38ed29c0274a91d0c439e671
HP Coder <andreimanzhola@gmail.com>
2022-12-22 16:50:59 +0200

    auth pages

---
+++ src/components/pages/SignIn.tsx
@@ -0,0 +1,85 @@
+import {
+  Box,
+  Card,
+  Stack,
+  TextInput,
+  PasswordInput,
+  Center,
+  Button,
+  Text,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import * as React from 'react';
+import { FormEventHandler } from 'react';
+import { useStyles } from '../../styles/authStyles';
+
+export interface IInitialFormValues {
+  emailOrPhone: string;
+  password: string;
+}
+
+const SignIn = (): JSX.Element => {
+  const { classes } = useStyles('signIn');
+
+  //* form
+  const form = useForm<IInitialFormValues>({
+    initialValues: {
+      emailOrPhone: '',
+      password: '',
+    },
+  });
+
+  //* submit
+  const onSubmit: FormEventHandler<HTMLFormElement> = form.onSubmit(values => {
+    console.log(values);
+  });
+
+  return (
+    <Box>
+      <Card px="2%" radius="md" className={classes.card}>
+        <Text className={classes.card_welcome_text}>You’re welcome! 👋</Text>
+        <Text className={classes.card_signup_text}>
+          Sign up your new account
+        </Text>
+        <form onSubmit={onSubmit}>
+          <Stack spacing="md">
+            <TextInput
+              {...form.getInputProps('emailOrPhone')}
+              size="md"
+              radius="md"
+              label="Your email or Phone"
+            />
+            <PasswordInput
+              {...form.getInputProps('password')}
+              size="md"
+              radius="md"
+              label="Password"
+            />
+          </Stack>
+          <Button
+            type="submit"
+            sx={{ marginTop: '8%' }}
+            className={classes.card_btn}
+          >
+            sign in
+          </Button>
+        </form>
+        <Center>
+          <Text className={classes.forget_password_text}>
+            Forgot your password?
+          </Text>
+        </Center>
+        <Center>
+          <Text className={classes.account_text}>
+            Already have an account?{' '}
+            <Text sx={{ color: '#625BF7' }} component="a" href="/signUp">
+              Sign up
+            </Text>
+          </Text>
+        </Center>
+      </Card>
+    </Box>
+  );
+};
+
+export default SignIn;
